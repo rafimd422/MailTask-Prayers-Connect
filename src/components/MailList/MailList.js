abc888ac4e7d29c653cee638e345b5c8ae50d9b7
rafimd422 <rafimd2222@gmail.com>
2024-01-17 17:30:07 +0600

    added images on the json

--- src/components/MailList/MailList.js
+++ src/components/MailList/MailList.js
@@ -7,7 +7,7 @@ import { Avatar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typ
           "senderEmail": "hackerrank@example.com",
           "subject": "Meeting Tomorrow",
           "time": "5 hours ago",
-          "image": "https://example.com/images/john_doe_avatar.jpg"
+          "image": "https://i.ibb.co/C0L7qfS/800px-Hacker-Rank-Icon-1000px.png"
         },
         {
           "id": "2",
@@ -15,7 +15,15 @@ import { Avatar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typ
           "senderEmail": "codeforces@example.com",
           "subject": "Project Update",
           "time": "12 hours ago",
-          "image": "https://example.com/images/alice_smith_avatar.jpg"
+          "image": "https://i.ibb.co/brX4qX3/codeforces-logo.png"
+        },
+        {
+          "id": "5",
+          "senderName": "Prayers Connect",
+          "senderEmail": "prayersconnect@example.com",
+          "subject": "Important Announcement",
+          "time": "3 days ago",
+          "image": "https://i.ibb.co/ykpYYdy/prayers-connect-logo.jpg"
         },
         {
           "id": "3",
@@ -23,7 +31,7 @@ import { Avatar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typ
           "senderEmail": "google@example.com",
           "subject": "Vacation Plans",
           "time": "1 day ago",
-          "image": "https://example.com/images/bob_jones_avatar.jpg"
+          "image": "https://i.ibb.co/Z2dH0kL/image8-2.webp"
         },
         {
           "id": "4",
@@ -31,15 +39,7 @@ import { Avatar, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Typ
           "senderEmail": "microsoft@example.com",
           "subject": "Important Announcement",
           "time": "2 days ago",
-          "image": "https://example.com/images/emily_white_avatar.jpg"
-        },
-        {
-          "id": "5",
-          "senderName": "Prayers Connect",
-          "senderEmail": "prayersconnect@example.com",
-          "subject": "Important Announcement",
-          "time": "2 days ago",
-          "image": "https://example.com/images/emily_white_avatar.jpg"
+          "image": "https://i.ibb.co/9wHS9GH/microsoft-logo.webp"
         }
       ]
 
@@ -56,7 +56,7 @@ return (
     <ListItem key={mail.id} disablePadding sx={{ background: 'white', borderRadius: '8px', marginBottom: '8px' }}>
       <ListItemButton>
         <ListItemIcon>
-        <Avatar alt={mail.senderName} src="/static/images/avatar/1.jpg" />
+        <Avatar alt={mail.senderName} src={mail.image} />
         </ListItemIcon>
         <ListItemText
           primary={mail.senderName}
